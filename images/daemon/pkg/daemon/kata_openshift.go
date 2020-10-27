4859b1f888c1980db77099c9a2a51061274a0942
Jens Freimann <jfreimann@redhat.com>
2020-10-27 14:10:44 +0100

    use payload image url from environment variable
    If set, use the environment variable KATA_PAYLOAD_IMAGE as
    the URL from where we download the container image with the
    RPMs that will be installed on the worker nodes.
    
    Signed-off-by: Jens Freimann <jfreimann@redhat.com>

--- images/daemon/pkg/daemon/kata_openshift.go
+++ images/daemon/pkg/daemon/kata_openshift.go
@@ -406,9 +406,18 @@ func installRPMs(k *KataOpenShift) (err error) {
 		fmt.Println(err)
 	}
 
-	srcRef, err := alltransports.ParseImageName("docker://quay.io/isolatedcontainers/kata-operator-payload:" + k.PayloadTag)
+	payloadImage := os.Getenv("KATA_PAYLOAD_IMAGE")
+	if payloadImage == "" {
+		payloadImage = "docker://quay.io/isolatedcontainers/kata-operator-payload:" + k.PayloadTag
+	} else {
+		log.Println("WARNING: kataconfig installation is tainted")
+		log.Println("Using env variable KATA_PAYLOAD_IMAGE " + payloadImage)
+		payloadImage = "docker://" + payloadImage
+	}
+
+	srcRef, err := alltransports.ParseImageName(payloadImage)
 	if err != nil {
-		fmt.Println("Invalid source name")
+		fmt.Println("Invalid source name of payload container image: " + payloadImage)
 		return err
 	}
 
