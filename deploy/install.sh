580db33defd25870afeb16d9f3bfedd0c1dbd0d9
Jens Freimann <jfreimann@redhat.com>
2021-05-05 10:22:14 +0200

    Update install.sh, use files from release-4.7 branch
    In install.sh we use other files and those should also come from the release-4.7 branch not from master. This patches fixes this.

--- deploy/install.sh
+++ deploy/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl https://raw.githubusercontent.com/openshift/kata-operator/master/deploy/deploy.sh | bash
-oc apply -f https://raw.githubusercontent.com/openshift/kata-operator/master/config/samples/kataconfiguration_v1_kataconfig.yaml
+curl https://raw.githubusercontent.com/openshift/kata-operator/release-4.7/deploy/deploy.sh | bash
+oc apply -f https://raw.githubusercontent.com/openshift/kata-operator/release-4.7/config/samples/kataconfiguration_v1_kataconfig.yaml
 
