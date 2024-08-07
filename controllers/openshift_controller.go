66c21b625515699f6442b45ac497a3229a102b6d
Beraldo Leal <bleal@redhat.com>
2024-08-07 14:01:42 -0400

    bump peerpod controllers to 0.9.0
    We are still using 0.8.0-alpha, and 0.9.0 changed layout.
    
    Signed-off-by: Beraldo Leal <bleal@redhat.com>

--- controllers/openshift_controller.go
+++ controllers/openshift_controller.go
@@ -24,7 +24,7 @@ import (
 	"reflect"
 	"time"
 
-	"github.com/confidential-containers/cloud-api-adaptor/peerpodconfig-ctrl/api/v1alpha1"
+	"github.com/confidential-containers/cloud-api-adaptor/src/peerpodconfig-ctrl/api/v1alpha1"
 
 	appsv1 "k8s.io/api/apps/v1"
 
