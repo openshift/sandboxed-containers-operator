b4ea8442ad7bddbed056aeaa5962ce27bec7d2bc
Julien Ropé <jrope@redhat.com>
2025-10-15 11:34:01 +0200

    operator-sdk: adding secure metrics serving
    While updating the operator-sdk, I'm finding some code that is supposed to be
    modified during upgrade, but doesn't exist in our repo.
    
    This commit allows to serve metrics using HTTPS instead of HTTP.
    It also disabled the HTTP2 method by default, to avoid some vulnerabilities.
    
    Found while looking at updates for operator-sdk 1.38 [0].
    Code sample found in operator-sdk testdata sources [1].
    
    [0] https://sdk.operatorframework.io/docs/upgrading-sdk-version/v1.38.0/
    [1] https://github.com/operator-framework/operator-sdk/blob/master/testdata/go/v4/memcached-operator/cmd/main.go
    
    Signed-off-by: Julien Ropé <jrope@redhat.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -18,6 +18,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"flag"
 	"os"
 
@@ -88,21 +89,48 @@ func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
+	var secureMetrics bool
+	var enableHTTP2 bool
+	var tlsOpts []func(*tls.Config)
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
+	flag.BoolVar(&secureMetrics, "metrics-secure", true,
+		"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
+	flag.BoolVar(&enableHTTP2, "enable-http2", false,
+		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
 	flag.Parse()
 
 	ctrl.SetLogger(zap.New(zap.UseDevMode(true), SetTimeEncoderToRfc3339()))
 
+	// if the enable-http2 flag is false (the default), http/2 should be disabled
+	// due to its vulnerabilities. More specifically, disabling http/2 will
+	// prevent from being vulnerable to the HTTP/2 Stream Cancellation and
+	// Rapid Reset CVEs. For more information see:
+	// - https://github.com/advisories/GHSA-qppj-fm5r-hxr3
+	// - https://github.com/advisories/GHSA-4374-p667-p6c8
+	disableHTTP2 := func(c *tls.Config) {
+		setupLog.Info("disabling http/2")
+		c.NextProtos = []string{"http/1.1"}
+	}
+
+	if !enableHTTP2 {
+		tlsOpts = append(tlsOpts, disableHTTP2)
+	}
+
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:  scheme,
-		Metrics: metricsserver.Options{BindAddress: metricsAddr},
+		Scheme: scheme,
+		Metrics: metricsserver.Options{
+			BindAddress:   metricsAddr,
+			SecureServing: secureMetrics,
+			TLSOpts:       tlsOpts,
+		},
 		WebhookServer: &webhook.DefaultServer{
 			Options: webhook.Options{
-				Port: 9443,
+				Port:    9443,
+				TLSOpts: tlsOpts,
 			},
 		},
 		HealthProbeBindAddress: probeAddr,
