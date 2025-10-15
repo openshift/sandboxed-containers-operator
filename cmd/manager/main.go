2a0a04c79c4f86855e9619811f297579db6ebbe4
Julien Ropé <jrope@redhat.com>
2025-10-15 11:15:58 +0200

    operator-sdk: adding HealthProbeBindAddress
    While updating the operator-sdk, I'm finding some code that is supposed to be
    modified during upgrade, but doesn't exist in our repo.
    
    This commit allows to configure health probe address instead of using the default.
    
    Found while looking at updates for operator-sdk 1.38 [0].
    Code sample found in operator-sdk testdata sources [1].
    
    [0] https://sdk.operatorframework.io/docs/upgrading-sdk-version/v1.38.0/
    [1] https://github.com/operator-framework/operator-sdk/blob/master/testdata/go/v4/memcached-operator/cmd/main.go
    
    Signed-off-by: Julien Ropé <jrope@redhat.com>

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -87,7 +87,9 @@ func SetTimeEncoderToRfc3339() zap.Opts {
 func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
+	var probeAddr string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
@@ -103,8 +105,9 @@ func main() {
 				Port: 9443,
 			},
 		},
-		LeaderElection:   enableLeaderElection,
-		LeaderElectionID: "290f4947.kataconfiguration.openshift.io",
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "290f4947.kataconfiguration.openshift.io",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
