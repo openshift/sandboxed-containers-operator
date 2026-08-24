61a316e9e6acb35472bddc98678da73f341a87d4
Julien Ropé <jrope@redhat.com>
2026-08-24 10:55:01 +0200

    claude: adding a watcher script for troubleshooting
    This script is using the same scripts than the skill, and shows the list of PRs
    that the skill will process, with their current status.
    
    It can help to keep an eye on the skill's activity over time, without jumping back
    and forth with a web browser's tabs.
    
    Signed-off-by: Julien Ropé <jrope@redhat.com>

---
+++ scripts/process-konflux-prs/watcher.sh
@@ -0,0 +1,15 @@
+# Call this (possibly with 'watch') to keep an eye on PRs that the skill will process
+jq -rs 'add' \
+    <(./snapshot-prs.sh --mintmaker | jq '[.[] | .+{type:"mintmaker"}]') \
+    <(./snapshot-prs.sh --nudge     | jq '[.[] | .+{type:"nudge"}]') |   { printf 'REPO\tPR #\tTYPE\tBRANCH\tOK2TEST\tLGTM\tCHECKS\tTITLE\n'; jq -r '.[] | [
+    .repo,
+    (.pr | tostring),
+    .type,
+    .base_branch,
+    (if .has_ok_to_test then "X" else "-" end),
+    (if .has_lgtm then "X" else "-" end),
+    (if (.pending_checks > 0) then "pending(\(.pending_checks))"
+     elif .build_checks_passed then "pass"
+     else "FAIL" end),
+    .title
+  ] | @tsv'; } |   column -t -s $'\t'
